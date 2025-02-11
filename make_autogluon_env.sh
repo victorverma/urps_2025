3546fe5a00089bc5c103fb3e518e50107816f423
victorverma <vishashv@gmail.com>
2025-02-10 22:24:15 -0500

    Use conda run, not conda activate

--- make_autogluon_env.sh
+++ make_autogluon_env.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 conda env create -p autogluon_env/ -f autogluon_env.yml
-conda activate autogluon_env/
-pip install catboost==1.2.7 --only-binary :all:
-uv pip install autogluon.timeseries
-conda deactivate
+conda run -p autogluon_env/ pip install catboost==1.2.7 --only-binary :all:
+conda run -p autogluon_env/ uv pip install autogluon.timeseries
