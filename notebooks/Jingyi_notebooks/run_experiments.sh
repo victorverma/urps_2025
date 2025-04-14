37849680371f99bd6975ea8af6030d1040ea7190
Jingyi Bai <kohaku@umich.edu>
2025-04-14 13:24:31 -0400

    Update run_experiments.sh 4/14

--- notebooks/Jingyi_notebooks/run_experiments.sh
+++ notebooks/Jingyi_notebooks/run_experiments.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-conda run -p ../../autogluon_env/ --live-stream python run_experiment_4.py \
-    --num_runs=50 \
-    --fve=0.9 \
-    --train_size=1000 \
+conda run -p ../../autogluon_env/ --live-stream python multi_model.py \
+    --num_runs=1000 \
+    --fve=0.5 \
+    --train_size=10000 \
     --prediction_length=100 \
     --eval_metric="RMSE" \
     --ci_level=0.95 \
-    --time_limit=300 \
-    --max_epochs=60 \
+    --time_limit=600 \
+    --max_epochs=1 \
     --verbosity=4 \
     --max_workers=$SLURM_NTASKS_PER_NODE
