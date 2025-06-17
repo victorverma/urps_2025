04f1b236a023db1bec8217fdc153f180c8b29811
Jingyi Bai <kohaku@umich.edu>
2025-06-17 08:54:57 +0800

    use better names for files

--- notebooks/Jingyi_notebooks/run_experiments.sh
+++ notebooks/Jingyi_notebooks/run_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-conda run -p ../../autogluon_env/ --live-stream python multi_model.py \
+conda run -p ../../autogluon_env/ --live-stream python run_experiments.py \
     --num_runs=1000 \
     --fve=0.5 \
     --train_size=10000 \
